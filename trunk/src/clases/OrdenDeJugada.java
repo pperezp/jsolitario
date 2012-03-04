db7fe3fe7a6bf3c94393bdff716d98b5d27e8c74
patodeath@gmail.com <patodeath@gmail.com@d19a19e7-84c0-2d41-6092-5668c8c497c3>
2012-03-04 20:29:18 +0000

    Primera vez

---
+++ trunk/src/clases/OrdenDeJugada.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package clases;
+
+/**
+ *
+ * @author pato
+ */
+public class OrdenDeJugada {
+    public static final int DESCENDIENTE = 0;
+    public static final int ASCENDENTE = 1;
+}
