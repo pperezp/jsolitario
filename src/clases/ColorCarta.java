19074daee4556fe297ec9ee851db61464dca7fcf
patodeath <patodeath@gmail.com>
2012-03-04 20:29:18 +0000

    Primera vez

---
+++ src/clases/ColorCarta.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package clases;
+
+/**
+ *
+ * @author pato
+ */
+public class ColorCarta {
+    public static final int AZUL = 0;
+    public static final int ROJA = 1;
+}
